0be971afd2561975d807d1d98f554f453b283b6c
Xientra <38855570+Xientra@users.noreply.github.com>
2020-09-18 14:18:53 +0200

    Made Item and Inventory Panel Movable

---
+++ Assets/Scripts/UI/MovableUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class MovableUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private bool moving = false;
+
+	private RectTransform rectTransform;
+	private Vector3 offset;
+
+	[Tooltip("If checked this RectTransform can only be dragged, when hovering over the handler")]
+	public bool onlyWorkWithHandle = true;
+	[Tooltip("A Garphic (image or similar) that functions as a handle to move this RectTransform")]
+	public Graphic handle;
+
+
+	private void Start()
+	{
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+	private void Update()
+	{
+		if (moving)
+		{
+			rectTransform.position = Input.mousePosition + offset;
+		}
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		if (eventData.pointerCurrentRaycast.gameObject == handle.gameObject)
+		{
+			offset = rectTransform.position - Input.mousePosition;
+			moving = true;
+		}
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		moving = false;
+	}
+}
